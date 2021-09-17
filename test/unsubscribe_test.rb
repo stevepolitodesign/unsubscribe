fe0e8010e44f561fa968626c66c792f5b5a5419f
Steve Polito <stevepolito@hey.com>
2021-09-17 10:15:04 -0400

    WIP: Write pending tests.

--- test/unsubscribe_test.rb
+++ test/unsubscribe_test.rb
@@ -4,4 +4,15 @@ class UnsubscribeTest < ActiveSupport::TestCase
   test "it has a version number" do
     assert Unsubscribe::VERSION
   end
+
+  test "can set mailers" do
+    assert Unsubscribe.respond_to?(:mailers)
+    assert Unsubscribe.respond_to?(:mailers=)
+  end
+
+  test "mailer should be an array" do
+    assert_raises(Unsubscribe::Error) do
+      Unsubscribe.mailers= "some_string"
+    end
+  end
 end
