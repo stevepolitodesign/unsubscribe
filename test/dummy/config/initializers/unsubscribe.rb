159bd2e2c43ca50d655443a95f89063ff4353787
Steve Polito <stevepolito@hey.com>
2021-09-17 19:58:19 -0400

    Remove comment.

--- test/dummy/config/initializers/unsubscribe.rb
+++ test/dummy/config/initializers/unsubscribe.rb
@@ -1,5 +1,3 @@
-Unsubscribe.setup do |config|
-  # FIXME: This is overriding the default value in my test
-  # rails test test/unsubscribe_test.rb:8 
+Unsubscribe.setup do |config| 
   config.mailers = ["MarketingMailer"]
 end
