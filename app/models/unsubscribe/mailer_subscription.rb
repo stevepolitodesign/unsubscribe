eca11a9062e6f6b4201d924634f12b2a0c06f001
Steve Polito <stevepolito@hey.com>
2021-09-18 10:34:29 -0400

    WIP

--- app/models/unsubscribe/mailer_subscription.rb
+++ app/models/unsubscribe/mailer_subscription.rb
@@ -1,5 +1,25 @@
 module Unsubscribe
   class MailerSubscription < ApplicationRecord
+    INCLUDED_MAILERS = Unsubscribe::SETTINGS.mailers.map{ |mailer| mailer[:class] }.freeze
+
     belongs_to :owner, polymorphic: true
+
+    before_validation :validate_configured_mailers
+
+    validates :subscribed, inclusion: [true, false], allow_nil: true
+    validates :mailer, presence: true
+    validates :mailer, inclusion: INCLUDED_MAILERS
+    validates :owner_id, uniqueness: { scope: [:mailer, :owner_type] }
+
+    def self.has_configured_mailers?
+      Unsubscribe::SETTINGS.mailers.present? && Unsubscribe::SETTINGS.mailers.map{ |mailer| mailer[:class] }.present?
+    end
+
+    private
+
+    def validate_configured_mailers
+      raise Unsubscribe::Error, "Please add mailers to config/initializers/unsubscribe.rb" unless Unsubscribe::MailerSubscription.has_configured_mailers?
+    end
+
   end
 end
--- test/models/unsubscribe/mailer_subscription_test.rb
+++ test/models/unsubscribe/mailer_subscription_test.rb
@@ -3,7 +3,12 @@ require "test_helper"
 module Unsubscribe
   class MailerSubscriptionTest < ActiveSupport::TestCase
     setup do
-      @owner = users(:one)
+      Unsubscribe.mailers = [{
+        class: "MarketingMailer",
+        name: "Marketing Emails",
+        description: "Updates on promotions and sales."        
+      }]
+      @owner = users(:two)
       @mailer_subscription = @owner.mailer_subscriptions.new(mailer: "MarketingMailer")
     end
 
@@ -12,7 +17,9 @@ module Unsubscribe
     end
 
     test "should be unique across owner and mailer" do
-      flunk
+      @mailer_subscription.save!
+      @duplicate_mailer_subscription = @owner.mailer_subscriptions.new(mailer: "MarketingMailer")
+      assert_not @mailer_subscription.valid?
     end
 
     test "should have a mailer" do
@@ -21,21 +28,11 @@ module Unsubscribe
     end
 
     test "mailer should be in list" do
-      valid_mailers = Unsubscribe.mailers = ["MarketingMailer"]
-      valid_mailers.each do |valid_mailer|
-        @mailer_subscription.mailer = valid_mailer
-        assert @mailer_subscription.valid?
-      end
-
       invalid_mailers = %w(Foo Bar Baz)
       invalid_mailers.each do |invalid_mailer|
         @mailer_subscription.mailer = invalid_mailer
         assert_not @mailer_subscription.valid?
       end
-    end
-
-    test "should raise exception if Unsubscribe.mailers is not an array" do
-      flunk
     end
 
     test "should be deleted when owner is deleted" do
