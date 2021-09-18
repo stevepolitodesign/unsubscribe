21e0e5bd88475215d84b60eb0d19ae51f9625de5
Steve Polito <stevepolito@hey.com>
2021-09-18 05:41:55 -0400

    Add pending tests.

--- test/models/unsubscribe/mailer_subscription_test.rb
+++ test/models/unsubscribe/mailer_subscription_test.rb
@@ -2,8 +2,40 @@ require "test_helper"
 
 module Unsubscribe
   class MailerSubscriptionTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    setup do
+      @mailer_subscription = MailerSubscription.new(mailer: "MarketingMailer")
+    end
+
+    test "should be valid" do
+      assert @mailer_subscription.valid?
+    end
+
+    test "should be unique across owner and mailer" do
+      flunk
+    end
+
+    test "should have a mailer" do
+      @mailer_subscription.mailer = nil
+      assert_not @mailer_subscription.valid?
+    end
+
+    test "mailer should be in list" do
+      valid_mailers = Unsubscribe.mailers = ["MarketingMailer"]
+      valid_mailers.each do |valid_mailer|
+        @mailer_subscription.mailer = valid_mailer
+        assert @mailer_subscription.valid?
+      end
+
+      invalid_mailers = %w(Foo Bar Baz)
+      invalid_mailers.each do |invalid_mailer|
+        @mailer_subscription.mailer = invalid_mailer
+        assert_not @mailer_subscription.valid?
+      end
+    end
+
+    test "should raise exception if Unsubscribe.mailers is not an array" do
+      flunk
+    end
+
   end
 end
