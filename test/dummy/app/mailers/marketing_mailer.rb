d2ca9dc8dddc163921e1ff09342d28495703e137
Steve Polito <stevepolito@hey.com>
2021-09-20 06:21:25 -0400

    Create test mailer.

---
+++ test/dummy/app/mailers/marketing_mailer.rb
@@ -0,0 +1,14 @@
+class MarketingMailer < ApplicationMailer  
+  unsubscribe_settings name: "Marketing Mailer", description: "Updates on promotions and sales."
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.marketing_mailer.promotion.subject
+  #
+  def promotion
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
---
+++ test/dummy/test/mailers/marketing_mailer_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class MarketingMailerTest < ActionMailer::TestCase
+  test "promotion" do
+    mail = MarketingMailer.promotion
+    assert_equal "Promotion", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+end
