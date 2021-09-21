792efb737c5fb354b17cd2cf9d9e527c9203cc79
Steve Polito <stevepolito@hey.com>
2021-09-21 07:24:59 -0400

    WIP: Build Controller.

--- app/controllers/unsubscribe/mailer_subscriptions_controller.rb
+++ app/controllers/unsubscribe/mailer_subscriptions_controller.rb
@@ -2,5 +2,36 @@ require_dependency "unsubscribe/application_controller"
 
 module Unsubscribe
   class MailerSubscriptionsController < ApplicationController
+    before_action :set_owner, only: [:show, :create]
+    before_action :set_mailer, only: [:show]
+
+    def show
+    end
+
+    def create
+      @mailer = Unsubscribe::MailerSubscription.new(mailer_subscription_params)
+
+      # TODO: redirect_to :show if @owner != @mailer.owner
+      if @mailer.save
+        redirect_to :show, notice: "Settings updated."
+      else
+        redirect_to :show, alert: @mailer.errors.full_messages.to_sentence
+      end
+    end
+
+    private
+
+    def set_owner
+      @owner = GlobalID::Locator.locate_signed(params[:id], for: :mailer_subscription)
+    end
+
+    def set_mailer
+      @mailer = @owner.mailer_subscriptions.find_or_initialize_by(mailer: params[:mailer])
+    end
+
+    def mailer_subscription_params
+      params.require(:mailer_subscription).permit(:owner_id, :owner_type, :subscribed, :mailer)
+    end
+
   end
 end
--- test/controllers/unsubscribe/mailer_subscriptions_controller_test.rb
+++ test/controllers/unsubscribe/mailer_subscriptions_controller_test.rb
@@ -4,13 +4,22 @@ module Unsubscribe
   class MailerSubscriptionsControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
 
+    setup do
+      @user = users(:two)
+    end
+
     test "should get" do
-      flunk
+      get mailer_subscription_path(@user.to_sgid(for: :mailer_subscription), mailer: "MarketingMailer")
+      assert_response :ok
     end
 
     test "should create" do
-      flunk
-    end
+      assert_difference("Unsubscribe::MailerSubscription.count", 1) do
+        post mailer_subscriptions_path, params: { id: @user.to_sgid(for: :mailer_subscription), mailer_subscription: { owner_id: @user.id } }
+      end
+      
+      assert_not @user.mailer_subscriptions.last.subscribed? 
+    end   
 
     test "should update" do
       flunk
