885ca83a729b6d6b8a810e1470e152db2fa6c9f7
Steve Polito <stevepolitodesign@users.noreply.github.com>
2021-09-23 22:35:25 -0400

    Set List-Unsubscribe header. (#14)

--- test/unsubscribe/mailer_test.rb
+++ test/unsubscribe/mailer_test.rb
@@ -26,5 +26,17 @@ module Unsubscribe
       end    
     end
 
+    test "should set headers" do
+      @mailer_subscription.toggle!(:subscribed)
+
+      email = MarketingMailer.with(
+        recipient: @user,
+      ).promotion
+
+      assert_not_nil email.header["List-Unsubscribe"]
+    end
+
   end
 end
+
+#<Mail::Message:13620, Multipart: true, Headers: <Date: Thu, 23 Sep 2021 22:26:06 -0400>, <From: from@example.com>, <To: two@example.com>, <Message-ID: <614d373e50942_362fbcc27aa@Steves-MBP.fios-router.home.mail>>, <Subject: Promotion>, <Mime-Version: 1.0>, <Content-Type: multipart/alternative; boundary="--==_mimepart_614d373e4fe49_362fbcc26b4"; charset=UTF-8>, <Content-Transfer-Encoding: 7bit>, <List-Unsubscribe: <http://example.com/unsubscribe/mailer_subscriptions/BAh7CEkiCGdpZAY6BkVUSSIfZ2lkOi8vZHVtbXkvVXNlci8yOTg0ODYzNzQGOwBUSSIMcHVycG9zZQY7AFQ6GG1haWxlcl9zdWJzY3JpcHRpb25JIg9leHBpcmVzX2F0BjsAVEkiHTIwMjEtMTAtMjRUMDI6MjY6MDYuMzE4WgY7AFQ=--fda2ec79d834aeba73324b278dacaa9d187d01bf?mailer=MarketingMailer>>>
