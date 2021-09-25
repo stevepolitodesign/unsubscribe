ec56e7e3fd9c841c7d72c0ff6f14ff12a9d2ab7c
Steve Polito <stevepolitodesign@users.noreply.github.com>
2021-09-24 22:33:47 -0400

    Fix long index ArgumentError (#19)
    * Name index to avoid exception.
    
    Fixes:
    
    ArgumentError: Index name 'index_unsubscribe_mailer_subscriptions_on_owner_type_and_owner_id' on table 'unsubscribe_mailer_subscriptions' is too long; the limit is 64 characters
    
    * Update index name.
    
    * Ignore migrations

--- db/migrate/20210918092925_create_unsubscribe_mailer_subscriptions.rb
+++ db/migrate/20210918092925_create_unsubscribe_mailer_subscriptions.rb
@@ -2,7 +2,7 @@ class CreateUnsubscribeMailerSubscriptions < ActiveRecord::Migration[6.0]
   def change
     create_table :unsubscribe_mailer_subscriptions do |t|
       # Needs to be polymorphic to be flexible. Owner may not always be a User.
-      t.references :owner, polymorphic: true, null: false
+      t.references :owner, polymorphic: true, null: false, index: { name: "unsubscribe_owner_index" }
       t.boolean :subscribed
       t.string :mailer, null: false
 
@@ -10,6 +10,6 @@ class CreateUnsubscribeMailerSubscriptions < ActiveRecord::Migration[6.0]
     end
 
     # An owner should only have one Unsubscribe::MailerSubscription record per Mailer.
-    add_index :unsubscribe_mailer_subscriptions, [:owner_id, :owner_type, :mailer], unique: true, name: :unsubscribe_owner_index
+    add_index :unsubscribe_mailer_subscriptions, [:owner_id, :owner_type, :mailer], unique: true, name: "unsubscribe_owner_mailer_index"
   end
 end
